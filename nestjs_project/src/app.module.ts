b819fe93f65463608df3fe729df57db77fe39180
Eslam-Nazer <eslam.nazer43@gmail.com>
2025-05-31 22:08:13 +0300

    Integrate with neon db postgressql and make employee crud

--- nestjs_project/src/app.module.ts
+++ nestjs_project/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import {UsersService} from "./users/users.service";
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
---
+++ nestjs_project/src/employees/employees.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from 'generated/prisma';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class EmployeesService {
+  constructor(private readonly db: DatabaseService) {}
+
+  async create(createEmployeeDto: Prisma.EmployeeCreateInput) {
+    return this.db.employee.create({
+      data: createEmployeeDto,
+    });
+  }
+
+  async findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.db.employee.findMany({
+        where: {
+          role,
+        },
+      });
+    }
+
+    return this.db.employee.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.db.employee.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updateEmployeeDto: Prisma.EmployeeUpdateInput) {
+    return this.db.employee.update({
+      where: {
+        id,
+      },
+      data: updateEmployeeDto,
+    });
+  }
+
+  async remove(id: number) {
+    return this.db.employee.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
